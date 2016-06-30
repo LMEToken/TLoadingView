48eb56d26c7804d290400961d31a901ff6754dad
谭伟平 <979000333@qq.com>
2016-06-30 10:19:35 +0800

    Initial Commit

---
+++ TLoadingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TLoadingView
+//
+//  Created by weiping.tan on 16/6/30.
+//  Copyright © 2016年 SSaaS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TLoadingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TLoadingView
+//
+//  Created by weiping.tan on 16/6/30.
+//  Copyright © 2016年 SSaaS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
